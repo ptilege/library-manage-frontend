2973bb150b86d9806406ea38771c1c7664bd3a7b
ptilege <ptilege@gmail.com>
2024-02-27 23:29:00 +0530

    binding data to frontend all books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
+import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent],
+  imports: [RouterOutlet,NavComponent,ViewAllBooksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-view-all-books',
+  standalone: true,
+  imports: [HttpClientModule, FormsModule, CommonModule],
+  templateUrl: './view-all-books.component.html',
+  styleUrl: './view-all-books.component.css'
+})
+export class ViewAllBooksComponent implements OnInit{
+
+  private http;
+  public bookList: any = {};
+  constructor(private httpClient:HttpClient){
+      this.http=httpClient;
+  }
+
+  ngOnInit(): void {
+      this.loadBooks();
+  }
+
+  loadBooks(){
+    this.http.get('http://localhost:8080/book/get').subscribe((data)=>{
+      console.log(data);
+      this.bookList = data;
+    })
+  }
+}
